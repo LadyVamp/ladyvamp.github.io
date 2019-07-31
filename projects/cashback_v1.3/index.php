7b79c2e7a72dd8bc021d95902808eb5e6c5a9125
Nadezhda Dyakova <ladyvamp666@gmail.com>
2019-07-31 17:36:26 +0300

    Update index.php

--- projects/cashback_v1.3/index.php
+++ projects/cashback_v1.3/index.php
@@ -1,19 +1,28 @@
 <!doctype html>
 <html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <title>CheckApp</title>
-  <!--   <base href="./"> -->
-  <base href="/projects/cashback_v1.3/">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/x-icon" href="favicon.ico">
-  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link rel="manifest" href="manifest.json">
-  <meta name="theme-color" content="#1976d2">
+    <meta charset="utf-8">
+    <title>CheckApp</title>
+    <!--   <base href="./"> -->
+    <base href="/projects/cashback_v1.3/">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="manifest" href="manifest.json">
+    <meta name="theme-color" content="#1976d2">
 </head>
+
 <body>
-  <app-root></app-root>
-  <noscript>Please enable JavaScript to continue using this application.</noscript>
-<script type="text/javascript" src="runtime.js"></script><script type="text/javascript" src="es2015-polyfills.js" nomodule></script><script type="text/javascript" src="polyfills.js"></script><script type="text/javascript" src="styles.js"></script><script type="text/javascript" src="vendor.js"></script><script type="text/javascript" src="main.js"></script></body>
+    <app-root></app-root>
+    <noscript>Please enable JavaScript to continue using this application.</noscript>
+    <script type="text/javascript" src="runtime.js"></script>
+    <script type="text/javascript" src="es2015-polyfills.js" nomodule></script>
+    <script type="text/javascript" src="polyfills.js"></script>
+    <script type="text/javascript" src="styles.js"></script>
+    <script type="text/javascript" src="vendor.js"></script>
+    <script type="text/javascript" src="main.js"></script>
+</body>
+
 </html>
